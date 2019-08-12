72bd558a1c07b7f788f7cce62475eb64d1e9a169
chenfeihao <chenfeihao@corp.netease.com>
2019-08-12 10:29:58 +0800

    @dev go 反射

---
+++ main/reflect_main.go
@@ -0,0 +1,7 @@
+package main
+
+import "let_me_go/reflect"
+
+func main() {
+	reflect.Testreflect()
+}
--- reflect/reflect.go
+++ reflect/reflect.go
@@ -1,1 +1,41 @@
 package reflect
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func (u User) Print(tag bool){
+	if tag {
+		fmt.Printf("my name is %s, i am %d years old\n", u.Name, u.Age)
+	} else {
+		fmt.Println("boronya can not say")
+	}
+}
+
+func Testreflect() {
+	user := User{"cfh", 1}
+	userType := reflect.TypeOf(user)
+	fmt.Println(userType)
+	userValue := reflect.ValueOf(user)
+	fmt.Println(userValue.Interface().(User).Name)
+	fmt.Println(userType.Field(0).Name)
+	fmt.Println(userType.Field(0).Type)
+	fmt.Println(reflect.ValueOf(&user).Elem().Field(0).CanSet())
+	fmt.Println(reflect.ValueOf(&user).Elem().Field(0).Interface())
+	reflect.ValueOf(&user).Elem().Field(0).SetString("yj")
+	fmt.Println(user)
+	bol := true
+	args := []reflect.Value{reflect.ValueOf(bol)}
+	reflect.ValueOf(&user).MethodByName("Print").Call(args)
+
+	var a float64 = 6.4
+	floatType := reflect.TypeOf(a)
+	fmt.Println(floatType)
+	fmt.Println(reflect.ValueOf(a).Interface())
+}
