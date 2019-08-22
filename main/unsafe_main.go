a951ef0b75c9b47c1ce7896bdb92baf1c0699da5
chenfeihao <chenfeihao@corp.netease.com>
2019-08-22 16:48:01 +0800

    @dev unsafe test

--- main/unsafe_main.go
+++ main/unsafe_main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"let_me_go/network"
+	"reflect"
 	"unsafe"
 )
 
@@ -19,4 +20,43 @@ func main() {
 	fmt.Println(*i)
 	*i = "192.168.1.1"
 	fmt.Println(*ip)
+
+	// 进行指针偏移
+	// 使用unsafe.Sizeof计算结构体每个成员变量所占的地址位
+	t := (*int32)(unsafe.Pointer(uintptr(unsafe.Pointer(ip)) + unsafe.Sizeof(ip.GetHost())))
+	fmt.Println(*t)
+	*t = 80
+	fmt.Println(*ip)
+
+	// 使用unsafe 进行string和slice的相互转换
+	s := "hello world"
+	bytes := string2Bytes(s)
+	for _, value := range bytes {
+		fmt.Println(value)
+	}
+
+	fmt.Println(bytes2String(bytes))
+}
+
+func string2Bytes(s string) []byte {
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&s))
+
+	sliceHeader := reflect.SliceHeader{
+		Data: stringHeader.Data,
+		Len:  stringHeader.Len,
+		Cap:  stringHeader.Len,
+	}
+
+	return *(*[]byte)(unsafe.Pointer(&sliceHeader))
+}
+
+func bytes2String(bytes []byte) string {
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&bytes))
+
+	stringHeader := reflect.StringHeader{
+		Data: sliceHeader.Data,
+		Len:  sliceHeader.Len,
+	}
+
+	return *(*string)(unsafe.Pointer(&stringHeader))
 }
--- network/network.go
+++ network/network.go
@@ -12,3 +12,11 @@ func (ip *IP) SetHost(host string)  {
 func (ip *IP) SetPort(port int32)  {
 	ip.port = port
 }
+
+func (ip *IP) GetPort() int32 {
+	return ip.port
+}
+
+func (ip *IP) GetHost() string {
+	return ip.host
+}
