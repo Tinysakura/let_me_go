f0e7182682015851a7c3dce24bc02a790184d757
chenfeihao <chenfeihao@corp.netease.com>
2019-08-25 16:47:34 +0800

    @dev grpcurl

--- main/grpc_server_main.go
+++ main/grpc_server_main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	proto "let_me_go/protobuf"
 	"log"
 	"net"
@@ -9,6 +10,8 @@ import (
 
 func main() {
 	grpcServer := grpc.NewServer()
+	// 注册grpcurl的reflection服务
+	reflection.Register(grpcServer)
 	proto.RegisterHelloServiceServer(grpcServer, new(proto.HelloServiceImpl))
 
 	listen, e := net.Listen("tcp", "localhost:8888")
